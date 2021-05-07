02fc5395b6119f0e671dbe71693d2dfda00f7291
chrism <chrism@kingorange.com>
2021-05-07 08:13:06 -0700

    pseudocode

---
+++ pseudo-code.py
@@ -0,0 +1,20 @@
+"""
+open file
+
+read thorugh every line in the file and put them into a list
+
+convert the values from the list into integers.
+
+iterate through the list
+
+sum_of_values = current_value + sum_of_vlaues
+number_of_values = number_of_values + 1
+average = sum_of_values / number_of values
+
+if current_value > average
+    values_over_average + 1
+
+values_over_average / number_of_values * 100
+
+round values_over_average
+"""
