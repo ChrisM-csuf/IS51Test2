294653ae4cbb02a8bea2fc3f4d65a65784db9db9
chrism <chrism@kingorange.com>
2021-05-07 08:12:07 -0700

    structured english

---
+++ structured-english.py
@@ -0,0 +1,9 @@
+"""
+get the list of grades from the provided file and convert them to type "int" and move through the list to add up all the
+numbers to get a sum of the dataset.
+Divide the sum by the total number of grades supplied in the file (number of lines)
+Determine how many values are above the average
+divide that value by the number of values.
+format as a perventage and round
+print output
+"""
